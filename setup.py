266353c7b76775fd04d7d82ab20e7f9d663d6038
Erwann LE PALLEC <155547074+erwann-lpc@users.noreply.github.com>
2025-11-20 11:14:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cisca",              
+    name="cisca",
     version="0.1.0",
-    packages=find_packages(include=["cisca", "cisca.*"]),
-)
+    packages=find_packages(),     #include all packages with init
+    include_package_data=True,     
